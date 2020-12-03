98178b54821a134538f02f4111b34b7e0ae2fa0e
Anjack <anjack@donkeywhisper.com>
2020-12-02 20:30:16 -0500

    Added CORS for Azure hosting support.

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -44,6 +44,11 @@ namespace dfe.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors((builder) =>
+            {
+                builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+            });
+
             app.UseResponseCompression();
 
             if (env.IsDevelopment())
